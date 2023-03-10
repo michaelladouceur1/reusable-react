73fb78217401d89c9aa94df2c94256a9140ef8f1
Michael LaDouceur <michaelladouceur1@gmail.com>
2023-03-09 23:44:53 -0600

    added handleSelection, selectedClass and useEffect to SelectionArea

--- src/components/SelectionArea.tsx
+++ src/components/SelectionArea.tsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import "./SelectionArea.css";
 
 export interface iSelectionArea {
 	selections: string[];
 	selected: string;
-	setSelected: () => void;
+	setSelected?: (selection: string) => void;
 	placeholder?: string;
 }
 
 export const SelectionArea: React.FC<iSelectionArea> = ({ selections, selected, setSelected, placeholder }) => {
+	const [uniqueSelections, setUniqueSelections] = useState(selections);
+
+	useEffect(() => {
+		const selectionsSet = new Set(selections);
+		setUniqueSelections(Array.from(selectionsSet));
+	}, [selections]);
+
+	const handleSelection = useCallback((selection: string) => {
+		if (setSelected) {
+			setSelected(selection);
+		}
+	}, []);
+
+	const selectedClass = useCallback(
+		(selection: string) => {
+			if (selection === selected) {
+				return "selection selected";
+			}
+			return "selection";
+		},
+		[selected]
+	);
+
 	return (
 		<div className="selection-area">
 			<input className="selected-value" />
 			<div className="selections">
-				{selections.map((selection, idx) => {
-					return (
-						<div key={`selection-${idx}`} className="selection">
-							{selection}
-						</div>
-					);
-				})}
+				{uniqueSelections &&
+					uniqueSelections.map((selection, idx) => {
+						return (
+							<div key={`selection-${idx}`} className={selectedClass(selection)} onClick={() => handleSelection(selection)}>
+								{selection}
+							</div>
+						);
+					})}
 			</div>
 		</div>
 	);
